890acc5c90bc4bb7a27acd53e3d0a8f9c781be16
Blue98 <167142631+Bllue98@users.noreply.github.com>
2025-02-20 22:28:06 -0300

    Front-End NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
+import NavBar from './components/layout/NavBar'
+// import BookCard from './components/BookCard'
 
-import BookCard from './components/BookCard'
-
-import capa_livro from './assets/cavernas_aco.jpg'
+// import capa_livro from './assets/cavernas_aco.jpg'
 
 function App() {
   
@@ -10,28 +10,6 @@ function App() {
     <>
       <div>
 
-        <h1>Pw3 - WEBAPP - LIVRARIA</h1>
-
-        <BookCard 
-        titulo='As Cavernas de Aço' 
-        autor='Isaac Azimov'
-        imagem={capa_livro}/>
-
-        {/* <BookCard 
-        titulo='Sol Desvelado' 
-        autor='Isaac Azimov'
-        imagem='Uma imagem vai aparecer aqui'/>
-
-        <BookCard 
-        titulo='O Fim da Infancia' 
-        autor='Arthur C. Clark'
-        imagem='Uma imagem vai aparecer aqui'/>
-
-         <BookCard 
-        titulo='Neuromancer' 
-        autor='Willian Gibson'
-        imagem='Uma imagem vai aparecer aqui'/> */}
-      
       </div>
     </>
   )
